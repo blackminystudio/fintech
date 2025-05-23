767d19b03d69b4a46b89e1058d818080da8c9491
Satyabrata Nayak <satyabratanayakofficial@gmail.com>
2025-05-23 12:56:33 +0530

    Riverpod and store setup

---
+++ lib/features/onboarding/store/onboarding_store.dart
@@ -0,0 +1,71 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../../profile/store/models/user_profile_model.dart';
+
+final userProfileProvider =
+    StateNotifierProvider<UserProfileStore, UserProfile>(
+  (ref) => UserProfileStore(),
+);
+
+class UserProfileStore extends StateNotifier<UserProfile> {
+  UserProfileStore() : super(const UserProfile());
+
+  // Onboarding Updates
+  void updateMobileNumber(String mobile) {
+    state = state.copyWith(info: state.info?.copyWith(mobileNumber: mobile));
+  }
+
+  void updateName(String name) {
+    state = state.copyWith(info: state.info?.copyWith(fullName: name));
+  }
+
+  //Other
+  void setAuthData(AuthData auth) {
+    state = UserProfile(uid: state.uid, auth: auth, info: state.info);
+  }
+
+  void updateUserInfo(UserInfo updatedInfo) {
+    state = UserProfile(
+      uid: state.uid,
+      auth: state.auth,
+      info: updatedInfo.copyWith(lastUpdated: DateTime.now()),
+    );
+  }
+
+  void updatePartialUserInfo({
+    String? mobileNumber,
+    String? fullName,
+    String? city,
+    String? gender,
+    String? maritalStatus,
+    DateTime? dateOfBirth,
+    String? monthlyIncome,
+    String? employmentStatus,
+  }) {
+    final newInfo = (state.info ?? const UserInfo()).copyWith(
+      mobileNumber: mobileNumber,
+      fullName: fullName,
+      city: city,
+      gender: gender,
+      maritalStatus: maritalStatus,
+      dateOfBirth: dateOfBirth,
+      monthlyIncome: monthlyIncome,
+      employmentStatus: employmentStatus,
+      lastUpdated: DateTime.now(),
+    );
+    state = state.copyWith(info: newInfo);
+  }
+}
+
+// extension on UserProfile {
+//   UserProfile copyWith({
+//     String? uid,
+//     AuthData? auth,
+//     UserInfo? info,
+//   }) =>
+//       UserProfile(
+//         uid: uid ?? this.uid,
+//         auth: auth ?? this.auth,
+//         info: info ?? this.info,
+//       );
+// }
