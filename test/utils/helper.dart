f9d5a6d3f3311de00e945437534275a1d31a77fd
Krushnakanta Nayak <krushnakanta678@gmail.com>
2025-06-24 12:26:51 +0530

    Onboarding TestCase Setup

---
+++ test/utils/helper.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+final theme = ThemeData.light();
+Future<void> pumpMinyWidgets(
+  WidgetTester tester, {
+  required Widget child,
+}) async {
+  final widgetScreen = ScreenUtilInit(
+    designSize: const Size(440, 956),
+    minTextAdapt: true,
+    builder: (context, _) => ProviderScope(
+      child: MaterialApp(
+        theme: ThemeData.light(),
+        home: Scaffold(body: child),
+      ),
+    ),
+  );
+  await tester.pumpWidget(widgetScreen);
+}
